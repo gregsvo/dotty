ddc27752b31e9e335f22b96420de36320b827b63
Greg Svoboda <gregsvo@gmail.com>
2018-03-28 17:30:51 -0400

    simplify config

--- capture/photo_capture.py
+++ capture/photo_capture.py
@@ -1,6 +1,5 @@
 import warnings
 from time import sleep
-from configparser import SafeConfigParser
 from picamera import PiCamera
 
 warnings.filterwarnings('default', category=DeprecationWarning)
@@ -9,21 +8,18 @@ warnings.filterwarnings('default', category=DeprecationWarning)
 def main(config):
         capture_photo(config)
 
-
 def capture_photo(config):
     with PiCamera() as camera:
         camera = PiCamera(resolution=(1920, 1080), framerate=30)
-        camera.iso = (config.get('cam_settings', 'iso'))
-        camera.resolution = (config.get('cam_settings', 'resolution'))
-        camera.framerate = (config.get('cam_settings', 'framerate'))
-        camera.brightness = (config.get('cam_settings', 'brightness'))
-        camera.contrast = (config.get('cam_settings', 'contrast'))
+        camera.iso = 100
+        camera.resolution = (1024, 768)
+        camera.framerate = 30
+        camera.brightness = 30
+        camera.contrast = 30
         # Camera warm-up time
         sleep(2)
         camera.capture('foo.jpg')
 
 
 if __name__ == '__main__':
-    config = SafeConfigParser()
-    config.read('config.ini')
-    main(config)
+    main()
